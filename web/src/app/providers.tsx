237c609d0e4c836d2059e4f7a1b3aba4e94854c1
Manas Pratim Biswas <manaspb2405@gmail.com>
2025-05-06 11:46:33 +0530

    [chore]: setup `posthog` for web
    Signed-off-by: Manas Pratim Biswas <manaspb2405@gmail.com>

--- web/src/app/providers.tsx
+++ web/src/app/providers.tsx
@@ -2,11 +2,14 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import { PostHogProvider } from "./PostHogProvider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <PostHogProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </PostHogProvider>
   );
 }
