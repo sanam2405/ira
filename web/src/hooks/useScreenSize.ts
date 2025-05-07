27337ae18f7d83573be83c6d51eaee50273a7a6a
Manas Pratim Biswas <manaspb2405@gmail.com>
2025-05-08 01:27:00 +0530

    [chore]: custom hook to determine media screen size
    Signed-off-by: Manas Pratim Biswas <manaspb2405@gmail.com>

---
+++ web/src/hooks/useScreenSize.ts
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+type ScreenSize = "mobile" | "tablet" | "desktop";
+
+export function useScreenSize() {
+  const [screenSize, setScreenSize] = useState<ScreenSize>("desktop");
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width < 768) {
+        setScreenSize("mobile");
+      } else if (width < 1024) {
+        setScreenSize("tablet");
+      } else {
+        setScreenSize("desktop");
+      }
+    };
+
+    // Initial check
+    handleResize();
+
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return screenSize;
+} 
